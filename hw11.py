39ba800f9b1bea5a4a57925a6660848a1cbb6402
Oleg Rubanov <165693738+orubanov@users.noreply.github.com>
2024-04-15 04:39:46 +0300

    Add files via upload

---
+++ hw11.py
@@ -0,0 +1,6 @@
+list_auto = ['BMW','MB','LADA','KIA','HONDA']
+cars_count = 0
+for i in range(len(list_auto)):
+    print('Я езжу на автомобиле марки', list_auto[i])
+    cars_count += 10
+    print(cars_count) #в задании нет ничего про ввывод, я так думаю тут он нужен
